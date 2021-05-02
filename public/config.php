f525c03a7fda278e11884cf111b3df9b3b18dbc0
jackson <jackson@pop-os.localdomain>
2021-05-02 18:51:41 -0300

    Refactor

---
+++ public/config.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+AditumPayments\ApiSDK\Configuration::initialize();
+
+AditumPayments\ApiSDK\Configuration::setUrl(AditumPayments\ApiSDK\Configuration::DEV_URL); // Caso não defina a url, será usada de produção
+AditumPayments\ApiSDK\Configuration::setCnpj("83032272000109");
+AditumPayments\ApiSDK\Configuration::setMerchantToken("mk_P1kT7Rngif1Xuylw0z96k3");
+
+print_r(AditumPayments\ApiSDK\Configuration::login());
